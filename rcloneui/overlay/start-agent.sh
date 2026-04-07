1802dea89cfa45f05ee560d4c5100f7ebb06ddf3
Richard Freitag <freitag@sunet.se>
2026-04-07 16:56:44 +0100

    Use /rclone/ as default location for rclone component

--- rcloneui/overlay/start-agent.sh
+++ rcloneui/overlay/start-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /usr/local/rclone-service
-rclone rcd --rc-web-gui --rc-no-auth --rc-web-gui-no-open-browser
+rclone rcd --rc-web-gui --rc-no-auth --rc-web-gui-no-open-browser --rc-baseurl rclone
 exit 0
